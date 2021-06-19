510e25d17cbe4bba3304a3eaacd59f49cc291e67
zumascahjengkonang@gmail.com <dimasdwipriyono231200@gmail.com>
2021-06-19 23:14:03 +0700

    first commit

--- app/src/main/java/com/example/jengkonangapp/jadwalronda.kt
+++ app/src/main/java/com/example/jengkonangapp/jadwalronda.kt
@@ -2,11 +2,15 @@ package com.example.jengkonangapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_home_layout.*
 
 class jadwalronda : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jadwalronda)
+
     }
 }
